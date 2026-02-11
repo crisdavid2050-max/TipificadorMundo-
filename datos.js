325fa856b071b042c2f891f08eeb6b2f101380bc
crisdavid2050-max <crisdavid2050@gmail.com>
2026-02-11 12:11:39 -0500

    Update datos.js

--- datos.js
+++ datos.js
@@ -1,4 +1,7 @@
+/* ===== SUB-SESIONES ===== */
+
 const observacionesPorSesion = {
+  "Manual": [],
   "Tareas cerradas": [],
   "Internet Despacho": [],
   "Televisión despacho": [],
@@ -6,3 +9,17 @@ const observacionesPorSesion = {
   "Persiste": [],
   "Sop N2": []
 };
+
+/* ===== LINKS PERMANENTES ===== */
+
+const linksImportantes = [
+  {
+    nombre: "Sondeo Google",
+    url: "https://docs.google.com/forms/d/e/1FAIpQLScOnqtEkPZTISXN4o3vrsi_vjMF3GcPuBlb0dIqJOuZVmeklQ/viewform"
+  },
+  {
+    nombre: "CRM",
+    url: "https://tusistema-crm.com"
+  }
+];
+
