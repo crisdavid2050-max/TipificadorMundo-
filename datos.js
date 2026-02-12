e1f152a74e27c7c1b64e60b250d17bce2ac15fbe
crisdavid2050-max <crisdavid2050@gmail.com>
2026-02-11 19:42:23 -0500

    Update datos.js

--- datos.js
+++ datos.js
@@ -1,4 +1,4 @@
-/* ===== SUB-SESIONES ===== */
+/* ================= SUB-SESIONES ================= */
 
 const observacionesPorSesion = {
   "Manual": [],
@@ -10,16 +10,17 @@ const observacionesPorSesion = {
   "Sop N2": []
 };
 
-/* ===== LINKS PERMANENTES ===== */
+/* ================= LINKS PERMANENTES ================= */
 
 const linksImportantes = [
-  {
-    nombre: "Sondeo Google",
-    url: "https://docs.google.com/forms/d/e/1FAIpQLScOnqtEkPZTISXN4o3vrsi_vjMF3GcPuBlb0dIqJOuZVmeklQ/viewform"
-  },
-  {
-    nombre: "CRM",
-    url: "https://tusistema-crm.com"
-  }
+  { nombre: "VICIDIAL", url: "https://cc.mundopacifico.cl/agc/vicidial.php?VD_login=1001047482&pl=5461c&pp=MuNd0P4c1F1c01nt3gr4" },
+  { nombre: "TV MUNDO", url: "https://cms.tvup.cloud/#/users" },
+  { nombre: "VINCULAR MUNDO GO", url: "https://www.mundogo.cl/link" },
+  { nombre: "RESTABLECER MUNDO GO", url: "https://www.mundogo.cl/recordar" },
+  { nombre: "PERSISTE", url: "https://docs.google.com/forms/d/e/1FAIpQLScBARUWj5MxH9pp9ax1QWFa-2voO9cx75yEE0q3qq_ZiD593Q/viewform" },
+  { nombre: "SONDEO", url: "https://docs.google.com/forms/d/e/1FAIpQLScOnqtEkPZTISXN4o3vrsi_vjMF3GcPuBlb0dIqJOuZVmeklQ/viewform" },
+  { nombre: "SPEEDTEST", url: "https://www.speedtest.net/es/result/" },
+  { nombre: "CONTINGENCIAS", url: "https://api-sigma.holdconet.com/contingencias-contact" },
+  { nombre: "CIERRE DE TAREAS", url: "https://docs.google.com/forms/d/e/1FAIpQLScABHhzzw4vq3ZRzFeJHhwSR4ulaciomBBe1qYuXTKqzlpMGA/viewform?usp=sf_link" },
+  { nombre: "FONDO CREA", url: "https://fondocrea.com.co/" }
 ];
-
